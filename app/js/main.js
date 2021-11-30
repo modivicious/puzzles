d15551d0af3a57b18344556078e088e835e0db58
modivicious <dens.2001@bk.ru>
2021-11-30 21:04:46 +0300

    added photos and lesson sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-
+  $(".photos__slider").slick({
+    dots: true,
+    prevArrow:
+      '<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><svg width="28" height="67" viewBox="0 0 28 67" fill="none"><path d="M27 66L1 33.5L27 1" stroke="#BFCBCF"/></svg></button>',
+    nextArrow:
+      '<button class="slick-next slick-arrow" aria-label="Next" type="button" style=""><svg width="28" height="67" viewBox="0 0 28 67" fill="none"><path d="M1 66L27 33.5L1 1" stroke="#BFCBCF"/></svg></button>',
+  });
 });
